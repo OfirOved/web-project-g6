8d08a7b7e0e56af7dc120529c9d03632a8ad2de7
lidayz <88537759+lidayz@users.noreply.github.com>
2021-08-30 14:24:09 +0300

    Add files via upload

---
+++ metargel.js
@@ -0,0 +1,36 @@
+// JavaScript source code
+
+function Rating(){
+	document.getElementById("Rating").style.fontSize ="50px";
+	document.getElementById("info").style.fontSize ="35px";
+	document.getElementById("Professions").style.fontSize ="35px";
+	document.getElementById("ContactUsEmail").style.fontSize ="35px";
+	document.getElementById("ContactUsphone").style.fontSize ="35px";
+	document.getElementById("Recommendations").style.fontSize ="35px";
+}
+
+function info(){
+	document.getElementById("info").style.fontSize ="50px";
+	document.getElementById("Rating").style.fontSize ="35px";
+	document.getElementById("Professions").style.fontSize ="35px";
+	document.getElementById("ContactUsEmail").style.fontSize ="35px";
+	document.getElementById("ContactUsphone").style.fontSize ="35px";
+	document.getElementById("Recommendations").style.fontSize ="35px";
+
+}
+
+function Professions(){
+	document.getElementById("Professions").style.fontSize ="50px";
+}
+
+function ContactUsEmail(){
+	document.getElementById("ContactUsEmail").style.fontSize ="50px";
+}
+function ContactUsphone(){
+	document.getElementById("ContactUsphone").style.fontSize ="50px";
+}
+
+function Recommendations(){
+	document.getElementById("Recommendations").style.fontSize ="50px";
+}
+
