83abdf33e7a3737bcc955a264f6aa561aa510a53
OfirOved <ofirov@post.bgu.ac.il>
2021-09-09 23:27:32 +0300

    forum done

---
+++ web-project-g6/BGUStudents/public/javascripts/HWforum.js
@@ -0,0 +1,11 @@
+function hide(){
+    document.getElementById("forum").style.display = "none";
+}
+
+function show(){
+    document.getElementById("forum").style.display = "block";
+}
+
+function upload(){
+    alert('פוסט נשלח בהצלחה!');
+}
