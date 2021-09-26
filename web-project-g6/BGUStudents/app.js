f9b04d05323da9c3de840d101d4cb8b805d29440
galyaviv <galyav@post.bgu.ac.il>
2021-09-26 11:28:36 +0300

    Convert to .pug, added initial routes

--- web-project-g6/BGUStudents/app.js
+++ web-project-g6/BGUStudents/app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const PORT = 3000;
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// set port, listen for requests
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,23 +24,81 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', function(req, res, next) {
+    res.render('HomePage');
+});
+
+app.get('/login', function(req, res, next) {
+    res.render('LogIn');
+});
+
+app.get('/aboutus', function(req, res, next) {
+    res.render('aboutUs');
+});
+
+app.get('/azraton/active', function(req, res, next) {
+    res.render('activeAzraton');
+});
+
+app.get('/azraton/manage', function(req, res, next) {
+    res.render('manageAzraton');
+});
+
+app.get('/committee', function(req, res, next) {
+    res.render('committee');
+});
+
+app.get('/contact', function(req, res, next) {
+    res.render('contactUs');
+});
+
+app.get('/documents', function(req, res, next) {
+    res.render('documents');
+});
+
+app.get('/grades', function(req, res, next) {
+    res.render('grades');
+});
+
+app.get('/hwforum', function(req, res, next) {
+    res.render('HWforum');
+});
+
+app.get('/map', function(req, res, next) {
+    res.render('map');
+});
+
+app.get('/mentors/search', function(req, res, next) {
+    res.render('MentorsSearch');
+});
+
+app.get('/mentors/profile', function(req, res, next) {
+    res.render('mentorProfile');
+});
+
+
+app.get('/marathon/new', function(req, res, next) {
+    res.render('newMarathon');
+});
+
+app.get('/yearly', function(req, res, next) {
+    res.render('yearlyPage');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
